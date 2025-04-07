83b5218df4fada80e787d636b5fced57f4147f9b
danvandeee <danvandeee@hotmail.com>
2025-04-07 21:47:18 +0200

    moved the usb logic to 2 seperate methods to clean up the code

--- src/bin/usb.rs
+++ src/bin/usb.rs
@@ -13,12 +13,12 @@
 // be linked)
 use panic_halt as _;
 
-use rp235x_hal::clocks::ClockSource;
+
+// Alias for our HAL crate
+use rp235x_hal as hal;
 use rp235x_hal::gpio::bank0::Gpio14;
 use rp235x_hal::gpio::{FunctionSio, Pin, PullDown, SioOutput};
 use rp235x_hal::timer::CopyableTimer0;
-// Alias for our HAL crate
-use rp235x_hal as hal;
 
 // Some things we need
 use core::fmt::Write;
@@ -93,10 +93,10 @@ fn main() -> ! {
     ));
 
     // Set up the USB Communications Class Device driver
-    let mut serial = SerialPort::new(&usb_bus);
+    let mut serial: SerialPort<'_, rp235x_hal::usb::UsbBus> = SerialPort::new(&usb_bus);
 
     // Create a USB device with a fake VID and PID
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+    let mut usb_dev: UsbDevice<'_, rp235x_hal::usb::UsbBus> = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
         .strings(&[StringDescriptors::default()
             .manufacturer("Danvandeee bv")
             .product("Serial port dan")
@@ -105,11 +105,7 @@ fn main() -> ! {
         .device_class(2) // from: https://www.usb.org/defined-class-codes
         .build();
 
-    let mut said_hello = false;
-
-
-
-   
+    let mut said_hello: bool = false;
 
     // Configure GPIO14 as an output
     let mut led: LedStatus = LedStatus::new(pins.gpio14.into_push_pull_output(), timer);
@@ -123,63 +119,86 @@ fn main() -> ! {
     let last_toggle_time = timer.get_counter().ticks();
     
     loop {
-        
-        // A welcome message at the beginning
-        if !said_hello && timer.get_counter().ticks() - last_toggle_time >= 8_000_000 {
-            said_hello = true;
-            let _ = serial.write(b"Hello, World!\r\n");
-
-            let time = timer.get_counter().ticks();
-            let mut text: String<64> = String::new();
-            writeln!(&mut text, "Current timer ticks: {}", time).unwrap();
-
-            // This only works reliably because the number of bytes written to
-            // the serial port is smaller than the buffers available to the USB
-            // peripheral. In general, the return value should be handled, so that
-            // bytes not transferred yet don't get lost.
-            let _ = serial.write(text.as_bytes());
-
-            led.set_high();
-        }
-
-        // Check for new data
-        if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 64];
-            match serial.read(&mut buf) {
-                Err(_e) => {
-                    // Do nothing
-                    led.set_high();
-                }
-                Ok(0) => {
-                    // Do nothing
-                }
-                Ok(count) => {
-                    // We have data to process
-                    // Blink the LED to show we are processing data
-                    led.set_high();
-
-                    // Convert to upper case
-                    buf.iter_mut().take(count).for_each(|b| {
-                        b.make_ascii_uppercase();
-                    });
-                    // Send back to the host
-                    let mut wr_ptr = &buf[..count];
-                    while !wr_ptr.is_empty() {
-                        match serial.write(wr_ptr) {
-                            Ok(len) => wr_ptr = &wr_ptr[len..],
-                            // On error, just drop unwritten data.
-                            // One possible error is Err(WouldBlock), meaning the USB
-                            // write buffer is full.
-                            Err(_) => break,
-                        };
-                    }
-                }
-            }
-        }
-
+        said_hello = usb_communication_say_hello(said_hello, last_toggle_time, &mut timer, &mut serial, &mut led);
+        usb_communication(&mut serial, &mut usb_dev, &mut led);
         led.set_low();
     }
 }
+
+fn usb_communication_say_hello(
+    said_hello: bool, 
+    last_toggle_time: u64, 
+    timer: &mut rp235x_hal::Timer<CopyableTimer0>, 
+    serial: &mut SerialPort<'_, rp235x_hal::usb::UsbBus>, 
+    led: &mut LedStatus) -> bool {
+
+    if said_hello {
+        return true;
+    }
+
+    // A welcome message at the beginning
+    if !said_hello && timer.get_counter().ticks() - last_toggle_time >= 8_000_000 {
+        let _ = serial.write(b"Hello, World!\r\n");
+
+        let time = timer.get_counter().ticks();
+        let mut text: String<64> = String::new();
+        writeln!(&mut text, "Current timer ticks: {}", time).unwrap();
+
+        // This only works reliably because the number of bytes written to
+        // the serial port is smaller than the buffers available to the USB
+        // peripheral. In general, the return value should be handled, so that
+        // bytes not transferred yet don't get lost.
+        let _ = serial.write(text.as_bytes());
+
+        led.set_high();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn usb_communication( 
+    serial: &mut SerialPort<'_, rp235x_hal::usb::UsbBus>, 
+    usb_dev: &mut UsbDevice<'_, rp235x_hal::usb::UsbBus>, 
+    led: &mut LedStatus) {
+
+    // Check for new data
+    if usb_dev.poll(&mut [ serial] ) {
+        let mut buf = [0u8; 64];
+        match serial.read(&mut buf) {
+            Err(_e) => {
+                // Do nothing
+                led.set_high();
+            }
+            Ok(0) => {
+                // Do nothing
+            }
+            Ok(count) => {
+                // We have data to process
+                // Blink the LED to show we are processing data
+                led.set_high();
+
+                // Convert to upper case
+                buf.iter_mut().take(count).for_each(|b| {
+                    b.make_ascii_uppercase();
+                });
+                // Send back to the host
+                let mut wr_ptr = &buf[..count];
+                while !wr_ptr.is_empty() {
+                    match serial.write(wr_ptr) {
+                        Ok(len) => wr_ptr = &wr_ptr[len..],
+                        // On error, just drop unwritten data.
+                        // One possible error is Err(WouldBlock), meaning the USB
+                        // write buffer is full.
+                        Err(_) => break,
+                    };
+                }
+            }
+        }
+    }
+}
+
+
 
 
 /// Program metadata for `picotool info`
