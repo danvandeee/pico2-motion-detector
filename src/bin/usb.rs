5c9a3f215f1a203f3b582ec3c16c1ed8efdc4fe9
danvandeee <danvandeee@hotmail.com>
2025-04-13 21:39:25 +0200

    put the usb communication in another module

--- src/bin/usb.rs
+++ src/bin/usb.rs
@@ -8,6 +8,8 @@ mod utils;
 use utils::led::CoolDownTime as CoolDownTime;
 use utils::led::LedStatus as LedStatus;
 
+use utils::usb_communication as usb_mod;
+
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
@@ -119,7 +121,7 @@ fn main() -> ! {
     let last_toggle_time = timer.get_counter().ticks();
     loop {
         said_hello = usb_communication_say_hello(said_hello, last_toggle_time, &mut timer, &mut serial, &mut led);
-        usb_communication(&mut serial, &mut usb_dev, &mut led);
+        usb_mod::usb_communication(&mut serial, &mut usb_dev, &mut led);
         led.repeat_if_needed();
     }
 }
@@ -137,65 +139,16 @@ fn usb_communication_say_hello(
 
     // A welcome message at the beginning
     if !said_hello && timer.get_counter().ticks() - last_toggle_time >= 8_000_000 {
-        write_serial_string(&"Hello, World!\r\n", serial);
+        usb_mod::write_serial_string(&"Hello, World!\r\n", serial);
 
         let time = timer.get_counter().ticks();
         let mut text: String<64> = String::new();
         writeln!(&mut text, "Current timer ticks: {}", time).unwrap();
-        write_serial_string(&text, serial);
+        usb_mod::write_serial_string(&text, serial);
         led.set_high();
         return true;
     } else {
         return false;
-    }
-}
-
-fn usb_communication(
-    serial: &mut SerialPort<'_, rp235x_hal::usb::UsbBus>, 
-    usb_dev: &mut UsbDevice<'_, rp235x_hal::usb::UsbBus>, 
-    led: &mut LedStatus) {
-
-    // Check for new data
-    if usb_dev.poll(&mut [ serial] ) {
-        let mut buf = [0u8; 64];
-        match serial.read(&mut buf) {
-            Err(_e) => {
-                // Do nothing
-                led.set_high();
-            }
-            Ok(0) => {
-                // Do nothing
-            }
-            Ok(count) => {
-                // We have data to process
-                // Blink the LED to show we are processing data
-                led.set_high();
-
-                // Convert to upper case
-                buf.iter_mut().take(count).for_each(|b| {
-                    b.make_ascii_uppercase();
-                });
-
-                write_serial_string(&"\r\nReceived: ", serial);
-                write_serial_data(&buf, serial, count);
-            }
-        }
-    }
-}
-
-fn write_serial_string(text: &str, serial: &mut SerialPort<'_, rp235x_hal::usb::UsbBus>) {
-    write_serial_data(text.as_bytes(), serial, text.len());
-}
-
-fn write_serial_data(buf: &[u8], serial: &mut SerialPort<'_, rp235x_hal::usb::UsbBus>, count: usize) {
-    let mut wr_ptr: &[u8] = &buf[..count];
-    while !wr_ptr.is_empty() {
-        match serial.write(wr_ptr) {
-            Ok(len) => wr_ptr = &wr_ptr[len..],
-            Err(_) => {
-                break;
-            },
-        };
     }
 }
 
